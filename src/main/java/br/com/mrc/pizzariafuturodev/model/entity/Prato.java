9ecfcdcf0d1347dc9352a8c004322c9ea7d86720
Matheus Rizzon Comoreto <matheusrcomoreto>
2022-10-29 22:27:26 -0300

    initial config

--- src/main/java/br/com/mrc/pizzariafuturodev/model/entity/Prato.java
+++ src/main/java/br/com/mrc/pizzariafuturodev/model/entity/Prato.java
@@ -1,7 +1,10 @@
 package br.com.mrc.pizzariafuturodev.model.entity;
 
+<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
+=======
+>>>>>>> fa5f0bfebf5fb8845de9aff6f6a8983d4c3c4965
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,10 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
+<<<<<<< HEAD
 import java.util.List;
+=======
+>>>>>>> fa5f0bfebf5fb8845de9aff6f6a8983d4c3c4965
 
 @Data
 @NoArgsConstructor
@@ -30,9 +36,12 @@ public class Prato {
 
     @ManyToOne
     private Categoria categoria;
+<<<<<<< HEAD
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @JsonBackReference
     @ManyToMany(mappedBy = "pedidoPrato")
     private List<Pedido> pedido;
+=======
+>>>>>>> fa5f0bfebf5fb8845de9aff6f6a8983d4c3c4965
 }
